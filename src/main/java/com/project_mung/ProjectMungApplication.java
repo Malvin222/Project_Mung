00bb3c402bbb1fe3579745b2e4249828fa9ed869
zzheek <zzjuheek@gmail.com>
2024-01-15 15:06:16 +0900

    Initial commit

---
+++ src/main/java/com/project_mung/ProjectMungApplication.java
@@ -0,0 +1,13 @@
+package com.project_mung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMungApplication.class, args);
+    }
+
+}
